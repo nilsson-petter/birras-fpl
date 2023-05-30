23b720cbb8e99355b8f1387d326f17dbc6d821a7
Petter Nilsson <petter.nilsson@avanza.se>
2023-05-30 22:29:12 +0200

    Some clean up

--- main.py
+++ main.py
@@ -5,33 +5,27 @@ import requests
 def get_players(league_id):
     url = "https://fantasy.premierleague.com/api/leagues-classic/{0}/standings/".format(league_id)
     print("Calling {}".format(url))
-    r = requests.get(url)
-    json = r.json()
-    result = dict()
-    return pd.DataFrame(json['standings']['results'])
+    return pd.DataFrame(requests.get(url).json()["standings"]["results"])
 
 def get_gw_result(gw, team_id):
     url = "https://fantasy.premierleague.com/api/entry/{0}/event/{1}/picks/".format(team_id, gw)
     print("Calling {}".format(url))
-    r = requests.get(url)
-    json = r.json()
-    return json['entry_history']
+    return requests.get(url).json()["entry_history"]
 
 
-league_id = 83736
+#league_id = 83736
+league_id = 881782
 
 players = get_players(league_id)
 gw_results = []
-for key in players['entry'].tolist():
-    for i in range(1, 39):
+for team_id in players['entry'].tolist():
+    for gw in range(1, 39):
         try:
-            #Exclude cancelled GW 7
-            if i != 7:
-                res = get_gw_result(i, key)
-                res['entry'] = key
-                gw_results.append(res)
+            res = get_gw_result(gw, team_id)
+            res['entry'] = team_id
+            gw_results.append(res)
         except:
-            print("Something went wrong for {0} {1}", key, i)
+            print("Something went wrong for {0} {1}".format( key, i))
 
 gw_results_df = pd.DataFrame(gw_results)
 
